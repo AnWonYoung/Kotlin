3f1969d019915a2aa98be84558391d9596832f85
AnWonYoung <ko1529@naver.com>
2021-10-05 15:05:35 +0900

    Kotlin if/for/while

--- app/src/main/java/com/example/kotlinexample1/KotlinBasic4.kt
+++ app/src/main/java/com/example/kotlinexample1/KotlinBasic4.kt
@@ -3,6 +3,7 @@ package com.example.kotlinexample1
 // Array
 // 1. List 2.MutableList (수정이 가능한 List)
 fun main() {
+    array()
     forAndWhile()
 }
 
@@ -26,9 +27,13 @@ fun array() {
 //    arrayList = arrayListOf() || Val cannot be reassigned 에러 발생
 //    본래의 주소값은 바뀌지 않으니 새롭게 초기화 할 수 없음
 
+    for(forArray in array) {
+        println("forArray의 값 : ${forArray}") // array 값 찍어보기
+    }
+
 }
 
-// ----------------반복문--------------------
+// ----------------6. 반복문--------------------
 
 fun forAndWhile() {
     val students = arrayListOf("won", "two", "three", "four")
@@ -37,7 +42,27 @@ fun forAndWhile() {
         println("${name}")
     }
 
+//  두 개 의 값을 할당 받아서 n 번째의 arrayListOf들을 하나씩 출력하기
+    for((index, name) in students.withIndex()) { // withIndex 함수를 함께 사용하기
+        println("${index+1}번째 학생 : ${name}")
+    }
+
+    var sum : Int = 0
     for( i : Int in 1..10) {
+        print("${i}")
+        sum += i
+    }
+    println("sum의 값 = ${sum}")
+
+    for( i : Int in 1..10 step 2) { // 2칸씩 13579
+        print("${i}")
+    }
+
+    for( i : Int in 10 downTo 1) { // 10부터 987654321 순으로
+        print("${i}")
+    }
+
+    for( i : Int in 1 until 100) { // 100을 포함하지 않음, 99번 돌아감 (1..100)은 100번 돌아감.
         print("${i}")
     }
 
@@ -46,6 +71,12 @@ fun forAndWhile() {
         for(j : Int in 1..9) {
             println("${i} * ${j} = ${i*j}")
         }
+    }
+//  while문 반복
+    var index = 0
+    while(index < 10) {
+        println("current index = ${index}")
+        index++
     }
 
 }
